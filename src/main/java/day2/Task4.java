e98bd65b3110d81f75bf328a662f009c5747bc68
pavel.vishnyakov <pashki4@gmail.com>
2022-04-15 12:44:17 +0300

    Add Day2 Task4 solution

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,21 @@
 package day2;
 
-public class Task4 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Task4 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double input = scanner.nextDouble();
+		double result;
+		if (input >= 5) {
+			result = (Math.pow(input, 2) - 10) / (input + 7);
+			System.out.println(result);
+		} else if (input > -3 && input < 5) {
+			result = (input + 3) * (Math.pow(input, 2) - 2);
+			System.out.println(result);
+		} else {
+			result = 420;
+			System.out.println(result);
+		}
+	}
 }
