60aa691ff97f451b694cc5f5b3165e77d1be333b
pavel.vishnyakov <pashki4@gmail.com>
2022-04-15 15:45:52 +0300

    Add Day3 Task1 solution

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,40 @@
 package day3;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Task1 {
+	public static void main(String[] args) {
+		boolean flag = true;
+		Scanner scanner = new Scanner(System.in);
+		while (flag) {
+			String input = scanner.nextLine();
+			switch (input) {
+				case "Москва" :
+				case "Владивосток" :
+				case "Ростов" :
+					System.out.println("Россия");
+					break;
+				case "Рим" :
+				case "Милан" :
+				case "Турин" :
+					System.out.println("Италия");
+					break;
+				case "Ливерпуль" :
+				case "Манчестер" :
+				case "Лондон" :
+					System.out.println("Англия");
+					break;
+				case "Берлин" :
+				case "Мюнхен" :
+				case "Кёльн" :
+					System.out.println("Германия");
+					break;
+				case "Stop" :
+					flag = false;
+					break;
+				default: 
+					System.out.println("Неизвестная страна");
+			}
+		}
+	}
 }
