a4f617a68846076c549140dc0c8bccbe71c91747
pavel.vishnyakov <pashki4@gmail.com>
2022-04-16 15:45:13 +0300

    Add Day4 Task3 and Task4 solution

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,49 @@
 package day4;
 
-public class Task3 {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
-    }
+public class Task3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int rowCount = scanner.nextInt();
+		int columnCount = scanner.nextInt();
+		int[][] array = new int[rowCount][columnCount];
+		settle2DArrayWithRandomNumbers(array);
+		print2DArray(array);
+		System.out.println("index of max row sum : " + indexOfRowMaxSum(array));
+
+	}
+	private static void print2DArray(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	private static void settle2DArrayWithRandomNumbers(int[][] array) {
+		Random random = new Random();
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = random.nextInt(50) + 0;
+			}
+		}
+	}
+	private static int indexOfRowMaxSum(int[][] array) {
+		int maxSum = 0;
+		int index = -1;
+		for (int i = 0; i < array.length; i++) {
+			int buffer = 0;
+			for (int j = 0; j < array[i].length; j++) {
+				buffer += array[i][j];
+			}
+			System.out.println(buffer);
+			if (buffer > maxSum) {
+				maxSum = buffer;
+				index = i;
+			}
+		}
+		return index;
+	}
 }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,35 @@
 package day4;
 
-public class Task4 {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
-    }
+public class Task4 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		int arrayLength = scanner.nextInt();
+		int maxValueOf3NearNumbers = 0;
+		int indexOfFirsOfThree = -1;
+
+		int[] array = new int[arrayLength];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = random.nextInt(10000) + 0;
+		}
+		System.out.println(Arrays.toString(array));
+
+		for (int i = 0; i < array.length - 2; i++) {
+			int buffer = 0;
+			for (int j = i; j <= i + 2; j++) {
+				buffer += array[j];
+			}
+			if (buffer > maxValueOf3NearNumbers) {
+				maxValueOf3NearNumbers = buffer;
+				indexOfFirsOfThree = i;
+			}
+		}
+		System.out.println(
+				"Max sum of near three numbers: " + maxValueOf3NearNumbers);
+		System.out.println("Index of first of three: " + indexOfFirsOfThree);
+	}
 }
