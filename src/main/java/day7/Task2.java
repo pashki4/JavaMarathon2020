0000eb1f36eb4193c53875d53c575503e259827b
pavel.vishnyakov <pashki4@gmail.com>
2022-04-17 23:11:24 +0300

    add day7 task1 and task2 solution

--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,15 @@
 package day7;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.util.Random;
 
-    }
+public class Task2 {
+	public static void main(String[] args) {
+		Player.info();
+		Random random = new Random();
+
+		for (int i = 0; i < 10; i++) {
+			Player player = new Player(random.nextInt(11) + 90);
+			Player.info();
+		}
+	}
 }
