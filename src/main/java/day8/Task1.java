80f5b6354573cbe9ba311ba004572e70934fb038
pavel.vishnyakov <pashki4@gmail.com>
2022-04-18 15:58:37 +0300

    add day8 task1 solution

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,27 @@
 package day8;
 
 public class Task1 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		System.out.println(stringConcat());
+		System.out.println(stringBuilder());
+	}
+	private static long stringBuilder() {
+		StringBuilder sb = new StringBuilder();
+		long start = System.nanoTime();
+		for (int i = 0; i <= 6621; i++) {
+			sb.append(i + " ");
+		}
+		System.out.println(sb);
+		return ((System.nanoTime() - start)/1_000_000);
+	}
+	public static long stringConcat() {
 
-    }
+		String result = "";
+		long start = System.nanoTime();
+		for (int i = 0; i <= 6621; i++) {
+			result = result + i + " ";
+		}
+		System.out.println(result);
+		return ((System.nanoTime() - start)/1_000_000);
+	}
 }
