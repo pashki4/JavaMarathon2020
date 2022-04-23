491c90640a301c1f25dc1bfdfe4adaa52231f9a5
pavel.vishnyakov <pashki4@gmail.com>
2022-04-23 23:16:53 +0300

    add day12 task5 solution

---
+++ src/main/java/day12/task5/MusicArtist.java
@@ -0,0 +1,27 @@
+package day12.task5;
+
+public class MusicArtist {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public MusicArtist(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "MusicArtist [name=" + name + ", age=" + age + "]";
+	}
+}
