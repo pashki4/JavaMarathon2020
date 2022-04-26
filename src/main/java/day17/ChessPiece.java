ec1c75b9abb6f78ba96f494cd6a91a2d1b24a366
pavel.vishnyakov <pashki4@gmail.com>
2022-04-26 19:31:35 +0300

    day17 task1 solution

---
+++ src/main/java/day17/ChessPiece.java
@@ -0,0 +1,30 @@
+package day17;
+
+public enum ChessPiece {
+	WHITE_KING("\u2654", 100),
+	WHITE_QUEEN("\u2655", 9),
+	WHITE_ROOK("\u2656",5),
+	WHITE_BISHOP("\u2657",3.5),
+	WHITE_KNIGHT("\u2658",3),
+	WHITE_PAWN("\u2659",1),
+	BLACK_KING("\u265A", 100),
+	BLACK_QUEEN("\u265B", 9),
+	BLACK_ROOK("\u265C",5),
+	BLACK_BISHOP("\u265D",3.5),
+	BLACK_KNIGHT("\u265E",3),
+	BLACK_PAWN("\u265F",1);
+	
+
+	private final String symbol;
+	private final double value;
+	
+	ChessPiece(String symbol, double value) {
+		this.symbol = symbol;
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return symbol;
+	}
+}
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,23 @@
 package day17;
 
 public class Task1 {
-    public static void main(String[] args) {
 
-    }
+	public static void main(String[] args) {
+		ChessPiece[] array = new ChessPiece[8];
+		for (int i = 0; i < 4; i++) {
+			array[i] = ChessPiece.WHITE_PAWN;
+			array[i] = ChessPiece.WHITE_PAWN;
+			array[i] = ChessPiece.WHITE_PAWN;
+			array[i] = ChessPiece.WHITE_PAWN;
+		}
+		for (int i = 4; i < 8; i++) {
+			array[i] = ChessPiece.BLACK_ROOK;
+			array[i] = ChessPiece.BLACK_ROOK;
+			array[i] = ChessPiece.BLACK_ROOK;
+			array[i] = ChessPiece.BLACK_ROOK;			
+		}
+		for (ChessPiece chessPiece : array) {
+			System.out.print(chessPiece + " ");
+		}
+	}
 }
