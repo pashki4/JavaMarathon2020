6bbbaaef2bafa666d15b292417abe9f22e94074b
pavel.vishnyakov <pashki4@gmail.com>
2022-04-26 11:29:56 +0300

    add day16 task1 solution

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,31 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		printResult(new File("numbers"));
+	}
 
-    }
+	public static void printResult(File file) {
+		StringBuilder sb = new StringBuilder();
+		double result = 0.0;
+		try {
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				sb.append(line + " ");
+			}
+			String[] split = sb.toString().trim().split(" ");
+			for (String num : split) {
+				result += Integer.parseInt(num);
+			}
+			result = result / split.length;
+			System.out.printf(result + " --> %.3f", result);
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+	}
 }
-
